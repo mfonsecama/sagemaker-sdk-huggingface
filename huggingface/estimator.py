34de598c1eb3a186d9ee5aedebff7bd566f01d2a
Philipp Schmid <schmidphilipp1995@gmail.com>
2020-12-24 13:39:34 +0100

    custom implementation of estimator

--- huggingface/estimator.py
+++ huggingface/estimator.py
@@ -1,12 +1,18 @@
+import logging
+
+logger = logging.getLogger("sagemaker")
+
 from sagemaker.estimator import Framework
 from sagemaker.pytorch.model import PyTorchModel
 from sagemaker.s3.S3Downloader import download as download_from_s3 , read_file as read_file_from_s3
+from utils import def validate_version_or_image_args
 import re
 
 
 class HuggingFace(Framework):
+    """Custom sagemaker-sdk estimator impelemntation of the HuggingFace libaries."""
     
-    _ecr_template_string = "public.ecr.aws/t6m7g5n4/huggingface-training:0.0.1-{device}-transformers{transformers_version}-datasets{datasets_version}"
+    _ecr_template_string = "public.ecr.aws/t6m7g5n4/huggingface-{type}:0.0.1-{device}-transformers{transformers_version}-datasets{datasets_version}"
     
     def __init__(
         self,
@@ -19,15 +25,43 @@ class HuggingFace(Framework):
         distributions=None,
         **kwargs
     ):
-      
-        self.image_name = self._get_container_image()
+        self.framework_version = framework_version
+        self.py_version = py_version
+        
+        validate_version_or_image_args(self.framework_version,self.py_version)
+        
+        self.image_name = self._get_container_image('training')
+        
+        #  for distributed training
+    #     if distribution is not None:
+    #     instance_type = renamed_kwargs(
+    #         "train_instance_type", "instance_type", kwargs.get("instance_type"), kwargs
+    #     )
+
+    #     validate_smdistributed(
+    #         instance_type=instance_type,
+    #         framework_name=self._framework_name,
+    #         framework_version=framework_version,
+    #         py_version=py_version,
+    #         distribution=distribution,
+    #         image_uri=image_uri,
+    #     )
+
+    #     warn_if_parameter_server_with_multi_gpu(
+    #         training_instance_type=instance_type, distribution=distribution
+    #     )
+
+    # if "enable_sagemaker_metrics" not in kwargs:
+    #     # enable sagemaker metrics for PT v1.3 or greater:
+    #     if self.framework_version and Version(self.framework_version) >= Version("1.3"):
+    #         kwargs["enable_sagemaker_metrics"] = True
+
       
         super(CustomFramework, self).__init__(
             entry_point, source_dir, hyperparameters, image_name=self.image_name, **kwargs
         )
     
-    def _configure_distribution(self, distributions):
-        return
+        # self.distribution = distribution or {}
     
     def upload_model_to_hub(self):
         return
@@ -39,10 +73,10 @@ class HuggingFace(Framework):
     # for distributed training
     #   return
     
-    def _get_container_image(self):
+    def _get_container_image(self,container_type):
       """return container image ecr url"""
       device = self._get_image_typ()
-      return _ecr_template_string.format(device=device,transformers_version=self.framework_version['transformers'],datasets_version=self.framework_version['datasets'])
+      return _ecr_template_string.format(device=device,transformers_version=self.framework_version['transformers'],datasets_version=self.framework_version['datasets'],type=container_type)
 
     def _get_container_device(self):
       """identifies container device """
@@ -82,5 +116,23 @@ class HuggingFace(Framework):
         https://sagemaker.readthedocs.io/en/stable/frameworks/pytorch/using_pytorch.html?highlight=requirements.txt#using-third-party-libraries
         there fore we has to include a `requirements.txt` to the code folder
         """
+        if "image_uri" not in kwargs:
+            kwargs["image_uri"] = self._get_container_image('inference')
+
+        kwargs["name"] = self._get_or_create_name(kwargs.get("name"))
         
-        return PyTorchModel()
+        return PyTorchModel(
+            self.model_data,
+            role or self.role,
+            entry_point or self._model_entry_point(),
+            framework_version='1.6',
+            py_version=self.py_version,
+            source_dir=(source_dir or self._model_source_dir()),
+            container_log_level=self.container_log_level,
+            code_location=self.code_location,
+            model_server_workers=model_server_workers,
+            sagemaker_session=self.sagemaker_session,
+            vpc_config=self.get_vpc_config(vpc_config_override),
+            dependencies=(dependencies or self.dependencies),
+            **kwargs
+        )
---
+++ huggingface/utils.py
@@ -0,0 +1,14 @@
+  def validate_version_or_image_args(framework_version, py_version):
+    """Checks if libary or python version arguments are specified correct.
+    Args:
+        framework_version (dict): Dictonary with 'transformers' and 'datasets' as key and their version of the library.
+        py_version (str): The version of Python.
+    Raises:
+        ValueError: if `framework_version['transfomers']` is None and `framework_version['datasets']` is None and `py_version` is
+            not 3.
+    """
+    if (framework_version is None or py_version is not '3' or 'transformers' not in framework_version or 'datasets' not in framework_version) and image_uri is None:
+        raise ValueError(
+            "framework_version or py_version was None, "
+            "Either specify both framework_version and py_version, or specify image_uri."
+        )
